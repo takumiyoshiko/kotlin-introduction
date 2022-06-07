8a4e44155a2883b4295e42b766a3431a34231cbc
takumiyoshiko <takumiyoshiko@gmail.com>
2022-06-07 23:26:38 +0900

    finish chapter2

---
+++ src/main/kotlin/chapter2/Game.kt
@@ -0,0 +1,12 @@
+package chapter2
+
+fun main(args: Array<String>) {
+    var hasSteed: Boolean = false
+    val playerName: String = "EStragon"
+    val pubName: String = "Unicorn's Horn"
+    val publicanName: String = "Daniel"
+    var gold: Int = 50
+    val drinks: List<String> = listOf("mead", "wine", "LaCroix")
+
+    println(playerName.reversed())
+}
--- src/main/kotlin/chapter2/TypeIntro.kt
+++ src/main/kotlin/chapter2/TypeIntro.kt
@@ -1,7 +1,10 @@
 package chapter2
 
+const val MAX_EXPERIENCE = 5000
 fun main(args: Array<String>) {
+    val playerName: String = "Estragon"
     var experiencePoints: Int = 5
     experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
 }
