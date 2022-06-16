c678be32678affa0fab09f5c959f58f4ddf23f91
takumiyoshiko <takumiyoshiko@gmail.com>
2022-06-16 23:16:15 +0900

    finish chapter5-12

--- src/main/kotlin/chapter5/SimVillage.kt
+++ src/main/kotlin/chapter5/SimVillage.kt
@@ -1,14 +1,22 @@
 package chapter5
 
 fun main(args: Array<String>) {
-    runSimulation("Guyal") { playerName, numBuildings ->
+    runSimulation("Guyal", ::printConstructionCost) { playerName, numBuildings ->
         val currentYear = 2018
         println("Adding $numBuildings")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
 
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(playerName: String,
+                         costPrinter: (Int) -> Unit,
+                         greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..3).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
