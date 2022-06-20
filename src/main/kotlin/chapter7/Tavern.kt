7c8d34c56be8c9e4caa24715cf254ec7bfc17434
takumiyoshiko <takumiyoshiko@gmail.com>
2022-06-20 09:58:10 +0900

    finish chapter7-5

---
+++ src/main/kotlin/chapter7/Tavern.kt
@@ -0,0 +1,32 @@
+package chapter7
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+fun main(args: Array<String>) {
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou] ")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
