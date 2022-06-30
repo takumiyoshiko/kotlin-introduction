da7360c7273fe7b79ea2f7ebaa5eef1e9dacda19
takumiyoshiko <takumiyoshiko@gmail.com>
2022-07-01 08:55:06 +0900

    finish chapter12-22

---
+++ src/main/kotlin/chapter12/Player.kt
@@ -0,0 +1,37 @@
+package chapter12
+
+import java.util.*
+
+class Player {
+    var name = "Madrigal"
+        get() = field.capitalize()
+        private set(value) {
+            field = value.trim()
+        }
+
+    var healthPoint = 89
+    val isBlessed = true
+    private val isImmortal = false
+
+    fun castFireball(numFireballs: Int = 2) =
+        println("A glass of fireball springs into existence. (x$numFireballs)")
+
+    fun formatHealthStatus() =
+        when (healthPoint) {
+            100 -> "is in excellent condition!"
+            in 90..99 -> "has a few scratches"
+            in 75..90 -> if (isBlessed) {
+                "has some minor wounds but is healing quite quickly!"
+            } else {
+                "has some minor wounds."
+            }
+            in 15..74 -> "looks pretty hurt"
+            else -> "is awful condition!"
+        }
+
+    fun auraColor(): String {
+        val auraVisible = isBlessed && healthPoint > 50 || isImmortal
+        val auraColor = if (auraVisible) "GREEN" else "NONE"
+        return auraColor
+    }
+}
