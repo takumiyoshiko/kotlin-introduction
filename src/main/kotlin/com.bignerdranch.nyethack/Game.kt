34f57ef9c0588f6af447a0dc0f3c9fdf11bffeae
takumiyoshiko <takumiyoshiko@gmail.com>
2022-07-05 09:47:02 +0900

    finish chapter15-10

--- src/main/kotlin/com.bignerdranch.nyethack/Game.kt
+++ src/main/kotlin/com.bignerdranch.nyethack/Game.kt
@@ -3,45 +3,52 @@ import com.bignerdranch.nyethack.TownSquare
 
 fun main(args: Array<String>) {
 
-    val player = Player(name = "Madrigal")
-    player.healthPoints = 100
-    player.castFireball()
-
-    var currentRoom: Room = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    //Aura
-    val auraColor = player.auraColor()
-
-    val healthStates = player.formatHealthStatus()
+    Game.play()
+}
 
 
-    printPlayerStatus(player)
 
-    var room = Room("Foyer")
-    println(room is Room)
-    println(room is TownSquare)
-
-    var townSquare = TownSquare()
-    println(townSquare is TownSquare)
-    println(townSquare is Room)
-
-    var className = when(townSquare) {
-        is Room -> "Room"
-        else -> throw java.lang.IllegalArgumentException()
+object Game {
+    private val player = Player(name = "Madrigal")
+    private var currentRoom: Room = TownSquare()
+    init {
+        println("Welcome, adventurer.")
+        player.castFireball()
     }
-    println(className)
-}
 
-private fun printPlayerStatus(
-    player: Player
-) {
-    println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("HP: ${player.healthPoints}, Hometown: ${player.homeTown}")
-    println("${player.name} ${player.formatHealthStatus()}")
-}
+    fun play() {
+        while (true) {
+            println(currentRoom.description())
+            println(currentRoom.load())
 
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+        }
+    }
+
+    private fun printPlayerStatus(
+        player: Player
+    ) {
+        println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("HP: ${player.healthPoints}, Hometown: ${player.homeTown}")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, { "" })
+
+        fun processCommand() = when(command.toLowerCase()) {
+            else -> commandNotFound()
+        }
+
+
+        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
+    }
+}
 
 
 
