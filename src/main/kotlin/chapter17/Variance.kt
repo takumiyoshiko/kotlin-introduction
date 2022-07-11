5dbaf75a084102b51346377f8ef7aa0d04ad7e77
takumiyoshiko <takumiyoshiko@gmail.com>
2022-07-11 09:49:35 +0900

    finish chapter17-17

--- src/main/kotlin/chapter17/Variance.kt
+++ src/main/kotlin/chapter17/Variance.kt
@@ -1,11 +1,11 @@
 package chapter17
 
-class Barrel<T>(var item: T)
+class Barrel<in T>(item: T)
 
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> =
         Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
-//    lootBarrel = fedoraBarrel
+    fedoraBarrel = lootBarrel
 }
