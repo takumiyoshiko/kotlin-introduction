c594952a87271899404754ef8dc0bbf2eebe4d65
takumiyoshiko <takumiyoshiko@gmail.com>
2022-07-12 09:52:14 +0900

    finish chapter18-9

---
+++ src/main/kotlin/chapter18/Extensions.kt
@@ -0,0 +1,17 @@
+package chapter18
+
+val String.numVowels
+    get() = count {"aeioy".contains(it)}
+
+fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
+
+fun <T> T.easyPrint(): T {
+    println(this)
+    return this
+}
+
+fun main(args: Array<String>) {
+    "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
+    42.easyPrint()
+    "How many vowels?".numVowels.easyPrint()
+}
