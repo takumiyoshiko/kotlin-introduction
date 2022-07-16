ce3fd6c61e422b78b6be1ab1ea2d04caef9c4aa2
takumiyoshiko <takumiyoshiko@gmail.com>
2022-07-16 20:41:34 +0900

    finish chapter20-3

---
+++ interop/src/main/java/org/example/Hero.kt
@@ -0,0 +1,6 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val adversary = Jhave()
+    println(adversary.utterGreeting())
+}
---
+++ interop/src/main/java/org/example/Jhave.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Jhave {
+    public String utterGreeting() {
+        return "BLARGH";
+    }
+}
