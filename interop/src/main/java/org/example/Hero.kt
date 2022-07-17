78bf53576ab2a50dd91a22f1a7b89c99f5bb9aca
takumiyoshiko <takumiyoshiko@gmail.com>
2022-07-17 22:02:19 +0900

    finish chapter20

--- interop/src/main/java/org/example/Hero.kt
+++ interop/src/main/java/org/example/Hero.kt
@@ -1,6 +1,58 @@
+@file:JvmName("Hero")
 package org.example
+
+import java.io.IOException
+
 
 fun main(args: Array<String>) {
     val adversary = Jhave()
     println(adversary.utterGreeting())
+
+    val friendshipLevel = adversary.determineFriendshipLevel()
+    println(friendshipLevel?.toLowerCase() ?: "It's complicated.")
+
+    val adversaryHitPoints: Int = adversary.hitPoints
+    println(adversaryHitPoints.dec())
+    println(adversaryHitPoints.javaClass)
+
+    adversary.greeting = "Hello, Hero."
+    println(adversary.utterGreeting())
+
+    adversary.offerFood()
+
+    try {
+        adversary.extendHandInFriendship()
+    } catch (e: Exception) {
+        println("Begone, foul beast!")
+    }
+
 }
+val translator = { utterance: String ->
+    println(utterance.toLowerCase().capitalize())
+}
+
+fun makeProclamation() = "Greetings, beast!"
+
+@JvmOverloads
+fun handOverFood(leftHand: String = "berries", rightHand: String = "beef") {
+    println("Mmmm... you hand over some delicious $leftHand and $rightHand.")
+}
+
+@Throws(IOException::class)
+fun acceptApology() {
+    throw IOException()
+}
+
+class Spellbook {
+    @JvmField
+    val spells = listOf("Magic Ms. L", "Lay on Hans")
+
+    companion object {
+        @JvmField
+        val MAX_SPELL_COUNT = 10
+
+        @JvmStatic
+        fun getSpellbookGreeting() = println("I am the Great Grimoire!")
+    }
+}
+
--- interop/src/main/java/org/example/Jhave.java
+++ interop/src/main/java/org/example/Jhave.java
@@ -1,7 +1,68 @@
 package org.example;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+
 public class Jhave {
+    private int hitPoints = 52489112;
+    private String greeting = "BLARGH";
+    @NotNull
     public String utterGreeting() {
-        return "BLARGH";
+        return greeting;
     }
+
+    @Nullable
+    public String determineFriendshipLevel() {
+        return null;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public void offerFood() {
+        Hero.handOverFood("pizza");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Hero.makeProclamation());
+
+        System.out.println("Spells: ");
+        Spellbook spellbook = new Spellbook();
+        for (String spell : spellbook.spells) {
+            System.out.println(spell);
+        }
+
+        System.out.println("Max spell count: " + Spellbook.MAX_SPELL_COUNT);
+
+        Spellbook.getSpellbookGreeting();
+
+        Function1<String, Unit> translator = Hero.getTranslator();
+        translator.invoke("TRUCE");
+    }
+
+    public void extendHandInFriendship() throws Exception {
+        throw new Exception();
+    }
+
+    public void apologize() {
+        try {
+            Hero.acceptApology();
+        } catch (IOException e) {
+            System.out.println("Caught!");
+        }
+    }
+
 }
